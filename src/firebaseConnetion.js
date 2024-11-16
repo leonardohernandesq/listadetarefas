aa9821dbd90dd67cb5e76757238ea8396f38fce0
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2024-11-16 12:35:33 +0000

    refactor project

--- src/firebaseConnetion.js
+++ src/firebaseConnetion.js
@@ -4,13 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD-fYid052i1eiAdNlvx9OkvRN0UiCP2IM",
-    authDomain: "cursosujeito-339cc.firebaseapp.com",
-    projectId: "cursosujeito-339cc",
-    storageBucket: "cursosujeito-339cc.appspot.com",
-    messagingSenderId: "226058249693",
-    appId: "1:226058249693:web:2e73b88aa9256c280b8234",
-    measurementId: "G-XEWRCS2DYY"
+    apiKey: "XXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXX",
+    projectId: "listadetarefas-XXXXXXXXX",
+    storageBucket: "listadetarefasXXXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"    
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
